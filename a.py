46b1c11f4cb72550aaedd7aa574b39f83b3c3877
s124202 <142102521+s124202@users.noreply.github.com>
2024-05-24 20:47:15 +0900

    a

--- a.py
+++ a.py
@@ -1,1 +1,14 @@
-2024/05/24
+import time
+
+def a():
+    x = 0
+    while True:
+        print(x)
+        time.sleep(1)
+        x = x + 1
+        if x == 100:
+            break
+    return 0
+
+if __name__ == '__main__':
+    a()

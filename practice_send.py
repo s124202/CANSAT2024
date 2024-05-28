0579688b4d7a32634fd9e958f28ae9190906661f
s124202 <142102521+s124202@users.noreply.github.com>
2024-05-28 20:46:14 +0900

    a

--- practice_send.py
+++ practice_send.py
@@ -76,10 +76,10 @@ def blt_send():
 
     bd_addr = "B8:27:EB:A9:5B:64"
     port = 1
-    while True:
-        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        sock.connect((bd_addr, port))
+    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    sock.connect((bd_addr, port))
 
+    while True:
         if center is not None:
             data = '1'
         else:

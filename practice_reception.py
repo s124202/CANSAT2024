26fb1046897830bae4509f699e8c02673ca977a9
s124202 <142102521+s124202@users.noreply.github.com>
2024-05-28 20:21:14 +0900

    初トライ

---
+++ practice_reception.py
@@ -0,0 +1,27 @@
+import bluetooth
+
+# Bluetoothポート番号
+port = 1
+
+# Bluetoothソケットを作成して接続を待機
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+sock.bind(("", port))
+sock.listen(1)
+print("Waiting for connection...")
+
+try:
+    while True:
+        client_sock, client_info = sock.accept()
+        print("Accepted connection from", client_info)
+
+        # データを受信
+        data = client_sock.recv(1024)
+        print("Received:", data)
+
+        # クライアントソケットをクローズ
+        client_sock.close()
+except KeyboardInterrupt:
+    print("Interrupted by user, closing socket...")
+
+# ソケットをクローズ
+sock.close()

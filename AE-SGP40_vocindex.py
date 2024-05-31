6ce5114b939c463f6f4ded42f66630fa419d853e
s1a4t9o <166107686+s1a4t9o@users.noreply.github.com>
2024-05-31 19:30:47 +0900

    a

---
+++ AE-SGP40_vocindex.py
@@ -0,0 +1,13 @@
+import time
+import board
+import adafruit_sgp40
+
+i2c = board.I2C() 
+sgp = adafruit_sgp40.SGP40(i2c)
+
+while True:
+    voc_index = sgp.measure_index()
+
+    print("VOC Index:", voc_index)
+    print("")
+    time.sleep(1)

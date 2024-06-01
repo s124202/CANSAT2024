1c630c2e44e28b07bb67a95b4935be6db268068c
s1a4t9o <166107686+s1a4t9o@users.noreply.github.com>
2024-06-01 15:46:12 +0900

    a

--- AE-SGP40_vocindex.py
+++ AE-SGP40_vocindex.py
@@ -1,9 +1,15 @@
 import time
 import board
 import adafruit_sgp40
+from adafruit_sgp40.voc_algorithm import (
+            VOCAlgorithm,
+        )
 
 i2c = board.I2C()
 sgp = adafruit_sgp40.SGP40(i2c)
+
+_voc_algorithm = VOCAlgorithm()
+_voc_algorithm.vocalgorithm_init()
 
 while True:
     raw = sgp.raw

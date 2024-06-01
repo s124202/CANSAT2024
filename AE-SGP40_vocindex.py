f91b20d77e6340da0c4ee1857dfb09dad821af3d
s1a4t9o <166107686+s1a4t9o@users.noreply.github.com>
2024-06-01 15:08:39 +0900

    a

--- AE-SGP40_vocindex.py
+++ AE-SGP40_vocindex.py
@@ -1,27 +1,12 @@
 import time
 import board
 import adafruit_sgp40
-import adafruit_bme280
 
-i2c = board.I2C()  # uses board.SCL and board.SDA
+i2c = board.I2C()
 sgp = adafruit_sgp40.SGP40(i2c)
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 while True:
-    temperature = bme280.temperature
-    humidity = bme280.relative_humidity
-
-    # For compensated raw gas readings
-    """
-    compensated_raw_gas = sgp.measure_raw(
-        temperature=temperature, relative_humidity=humidity
-    )
-    print("Raw Data:", compensated_raw_gas)
-    """
-
-    # For Compensated voc index readings
-    voc_index = sgp.measure_index(
-    temperature=temperature, relative_humidity=humidity)
+    voc_index = sgp.measure_index()
 
     print("VOC Index:", voc_index)
     print("")

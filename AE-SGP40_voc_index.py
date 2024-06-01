5ad2bfa0eee2dfec9ccd80c2952b596116bc3350
s1a4t9o <166107686+s1a4t9o@users.noreply.github.com>
2024-06-01 20:47:48 +0900

    a

---
+++ AE-SGP40_voc_index.py
@@ -0,0 +1,21 @@
+import time
+import board
+import adafruit_sgp40
+import BME280
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+sgp = adafruit_sgp40.SGP40(i2c)
+BME280.bme280_calib_param()
+BME280.bme280_setup()
+
+while True:
+    data = BME280.bme280_read()
+    temperature = data[0]
+    humidity = data[3]
+
+    voc_index = sgp.measure_index(
+    temperature=temperature, relative_humidity=humidity)
+
+    print("VOC Index:", voc_index)
+    print("")
+    time.sleep(1)

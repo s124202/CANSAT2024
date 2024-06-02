08c8fc06a69e2c6116b284899807cb4f24c0ff3d
s1a4t9o <166107686+s1a4t9o@users.noreply.github.com>
2024-06-02 18:48:12 +0900

    a

--- sato_test.py
+++ sato_test.py
@@ -1,35 +1,8 @@
-import time
-import board
-import adafruit_sgp40
 import BME280
 
-i2c = board.I2C()  # uses board.SCL and board.SDA
-sgp = adafruit_sgp40.SGP40(i2c)
 BME280.bme280_calib_param()
 BME280.bme280_setup()
 
+a = BME280.bme280_read()
 
-data = BME280.bme280_read()
-temperature = data[0]
-humidity = data[3]
-temperature=temperature
-relative_humidity=humidity
-
-def measure_index(
-        temperature: float = 25, relative_humidity: float = 50
-    ) -> int:
-        
-        raw = measure_raw(temperature, relative_humidity)
-        if raw < 0:
-            return -1
-        else:
-            return raw
-        
-if __name__ == '__main__':
-    try:
-        a = measure_index()
-        print(a)
-
-    except Exception as e:
-        print()
-        print(e.message)
+print(a)

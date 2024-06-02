d665d239a3086001cf066f85455fd3358100f704
s1a4t9o <166107686+s1a4t9o@users.noreply.github.com>
2024-06-03 05:38:33 +0900

    a

--- AE-SGP40_voc_index.py
+++ AE-SGP40_voc_index.py
@@ -8,29 +8,12 @@ sgp = adafruit_sgp40.SGP40(i2c)
 BME280.bme280_calib_param()
 BME280.bme280_setup()
 
-def measure_index(
-		temperature: float = 25, relative_humidity: float = 50
-	) -> int:
-		
-		from adafruit_sgp40.voc_algorithm import (
-			VOCAlgorithm,
-		)
-		
-		_voc_algorithm = VOCAlgorithm()
-		_voc_algorithm.vocalgorithm_init()
-
-		sraw = sgp.measure_raw(temperature, relative_humidity)
-		if sraw < 0:
-			return -1
-		voc_index = _voc_algorithm.vocalgorithm_process(sraw)
-		return voc_index
-
 while True:
 	data = BME280.bme280_read()
 	temperature = data[0]
 	humidity = data[3]
 
-	voc_index = measure_index(
+	voc_index = sgp.measure_index(
 	temperature=temperature, relative_humidity=humidity)
 
 	print("VOC Index:", voc_index)

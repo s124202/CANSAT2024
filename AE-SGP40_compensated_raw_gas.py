0e27c5f334a6a5b82b87d6fc9b7667e12b4dbda0
s1a4t9o <166107686+s1a4t9o@users.noreply.github.com>
2024-06-14 16:28:09 +0900

    a

--- AE-SGP40_compensated_raw_gas.py
+++ AE-SGP40_compensated_raw_gas.py
@@ -1,16 +1,16 @@
 import time
 import board
 import adafruit_sgp40
-import BME280
+import bme280
 
 i2c = board.I2C() 
 sgp = adafruit_sgp40.SGP40(i2c)
-BME280.bme280_calib_param()
-BME280.bme280_setup()
+bme280.bme280_calib_param()
+bme280.bme280_setup()
 
 try:
 	while True:
-		data = BME280.bme280_read()
+		data = bme280.bme280_read()
 		temperature = data[0]
 		humidity = data[2]
 

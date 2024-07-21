8445923abd25a54d5f06298edbdaa5337652bcc0
s1a4t9o <166107686+s1a4t9o@users.noreply.github.com>
2024-07-21 17:52:28 +0900

    　

--- para_fall_test.py
+++ para_fall_test.py
@@ -7,10 +7,7 @@ import stuck_sato
 motor.setup()
 
 print("Improving the rover's posture")
-stuck.correct_posture()
-
-#print("Improving the rover's posture")
-#stuck.correct_posture()
+stuck_sato.correct_posture()
 
 print("#####-----Parachute Avoid Sequence: Start-----#####")
 
@@ -18,22 +15,6 @@ print("Para Avoid Start")
 
 check_count = 0 #パラ回避用のカウンター
 red_area = 0
-
-PARA_THD_COVERED = 69120*0.7
-
-red_area = para_avoidance.detect_red()
-
-if red_area > PARA_THD_COVERED:
-    print("Parachute on top")
-    motor.move(80, 80, 5)
-elif red_area == 0:
-    print("Move Forwward")
-    motor.move(60, 60, 5) #徐々に減速するはず
-    #motor.motor_stop(0.2)
-else:
-    print("Move Backwward")
-    motor.move(-60, -60, 5) #徐々に減速するはず
-    #motor.motor_stop(0.2)
 
 PARA_THD_COVERED = 255000
 

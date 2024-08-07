cddba12a109580c7916e1f44e2a94bc3a14a7dfa
s124202 <s.kai.124202@gmail.com>
2024-08-07 22:50:16 +0900

    　

--- purple_detection_test.py
+++ purple_detection_test.py
@@ -1,22 +1,22 @@
 import cv2
 import numpy as np
-import take_photo
+import time
 
-def detect_purple(small_img):
+def detect_purple(img):
 	# HSV色空間に変換
-	hsv = cv2.cvtColor(small_img, cv2.COLOR_BGR2HSV)
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-	# 赤色のHSVの値域1
-	hsv_min = np.array([0,50,50])
-	hsv_max = np.array([5,255,255])
+	# 紫色のHSVの値域
+	hsv_min = np.array([110,50,0])
+	hsv_max = np.array([130,210,255])
 	mask = cv2.inRange(hsv, hsv_min, hsv_max)
 
 	return mask
 
-def mosaic(original_img, ratio):
-	small_img = cv2.resize(original_img, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+def mosaic(img, ratio):
+	small_img = cv2.resize(img, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
 	
-	return cv2.resize(small_img, original_img.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+	return cv2.resize(small_img, img.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
 
 def get_max_contour(mask, img):
 	try:
@@ -32,7 +32,7 @@ def get_max_contour(mask, img):
 	
 	return img, max_contour
 
-def get_para_area(max_contour, para_img):
+def get_para_area(max_contour):
 	try:
 		# 輪郭の面積を計算
 		area = cv2.contourArea(max_contour)
@@ -41,67 +41,19 @@ def get_para_area(max_contour, para_img):
 
 	return area
 
-def get_center(mask, original_img):
-	try:
-		contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-		
-		# 最大の輪郭を抽出
-		max_contour = max(contours, key = cv2.contourArea)
-
-		# 最大輪郭の重心を求める
-		# 重心の計算
-		m = cv2.moments(max_contour)
-		cx,cy= m['m10']/m['m00'] , m['m01']/m['m00']
-		# print(f"Weight Center = ({cx}, {cy})")
-		# 座標を四捨五入
-		cx, cy = round(cx), round(cy)
-		# 重心位置に x印を書く
-		cv2.line(original_img, (cx-5,cy-5), (cx+5,cy+5), (0, 255, 0), 2)
-		cv2.line(original_img, (cx+5,cy-5), (cx-5,cy+5), (0, 255, 0), 2)
-
-		cv2.drawContours(original_img, [max_contour], -1, (0, 255, 0), thickness=2)
-
-	except:
-		max_contour = 0
-		cx = 0
-		cy = 0
-	
-	return original_img, max_contour, cx, cy
-
-def get_angle(cx, cy, original_img):
-	angle = 0
-	# 重心から現在位置とゴールの相対角度を大まかに計算
-	img_width = original_img.shape[1]
-	quat_width = img_width / 5
-	x0, x1, x2, x3, x4, x5 = 0, quat_width, quat_width*2, quat_width*3, quat_width*4, quat_width*5
-
-	if x0 < cx <x1:
-		angle = 1
-	elif x1 < cx < x4:
-		angle = 2
-	elif x4 < cx < x5:
-		angle = 3
-
-	return angle
-
-def detect_para():
-	path_all_para = 'photo/detect_para/'
-	photoname = take_photo.Capture(path_all_para)
-	para_img = cv2.imread(photoname)
-
-	#画像を圧縮
-	small_img = mosaic(para_img, ratio=0.8)
-
-	#赤色であると認識させる範囲の設定
-	mask = detect_purple(small_img)
-
-	#圧縮した画像から重心と輪郭を求めて、画像に反映
-	para_img, max_contour, cx, cy = get_center(mask, small_img)
-
-	#赤色が占める割合を求める
-	purple_area = get_para_area(max_contour, para_img)
-
-	return(purple_area)
+#def get_larger_purple_object(mask):
+#	# 最小領域の設定
+#	minarea = 50
+#	nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask)
+#	if nlabels > 1:
+#		largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
+#		center = centroids[largest_label]
+#		size = stats[largest_label,cv2.CC_STAT_AREA]
+#		if size > minarea:
+#			return center, size
+#		return None, 0
+#	else:
+#		return None, 0
 
 def detect_para_movie():
 	# カメラのキャプチャ
@@ -142,6 +94,30 @@ def detect_para_movie():
 
 	# すべてのウィンドウを閉じる
 	cv2.destroyAllWindows()
+
+def detect_para():
+	# カメラのキャプチャ
+	cap = cv2.VideoCapture(0)
+
+	for i in range(5):
+		# フレームを取得
+		ret, frame = cap.read()
+
+		# カメラ表示を180度回転
+		frame = cv2.rotate(frame, cv2.ROTATE_180)
+
+		# 画像を圧縮
+		frame = mosaic(frame, ratio=0.8)
+
+		# 赤色検出
+		mask = detect_purple(frame)
+		frame, max_contour = get_max_contour(mask, frame)
+		purple_area = get_para_area(max_contour)
+
+	# カメラを閉じる
+	cap.release()
+
+	return(purple_area)
 
 if __name__ == '__main__':
 	detect_para_movie()
